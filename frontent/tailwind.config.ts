0095f359af89a9cb4fb633ce380ae39a2be867d4
Rakibul Islam <rakibul.islam7772588@gmail.com>
2025-02-21 23:27:21 +0600

    add linter and formatter

--- frontent/tailwind.config.ts
+++ frontent/tailwind.config.ts
@@ -1,22 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				background: "var(--background)",
-				foreground: "var(--foreground)",
-			},
-			container: {
-				center: true,
-				padding: "1.2rem",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      container: {
+        center: true,
+        padding: '1.2rem',
+      },
+    },
+  },
+  plugins: [],
 } satisfies Config;
